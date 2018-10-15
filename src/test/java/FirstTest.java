9b1363f19d316802e6acca6ac8483b97a5678519
gctrack2009@gmail.com <Green@18>
2018-10-14 20:49:51 -0400

    First Git - Commit 10/13

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -23,31 +25,33 @@ public class FirstTest {
     @BeforeMethod
     public void setupTest (){
         //Create a new ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\anily\\IdeaProjects\\seleniumAutomation\\src\\main\\resources\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-
+      //  ChromeOptions chromeOptions = new ChromeOptions();
+      //  chromeOptions.setBinary("C:\\Users\\anily\\IdeaProjects\\seleniumAutomation\\src\\main\\resources\\drivers\\chromedriver.exe");
+       // System.setProperty("webdriver.chrome.driver", "C:\\Users\\anily\\IdeaProjects\\seleniumAutomation\\src\\main\\resources\\drivers\\chromedriver.exe");
+       // driver = new ChromeDriver();
+        System.out.println("Hello World");
 
         //Go to www.swtestacademy.com
-        driver.navigate().to(testURL);
+      //  driver.navigate().to(testURL);
     }
 
     //-----------------------------------Tests-----------------------------------
     @Test
     public void firstTest () {
         //Get page title
-        String title = driver.getTitle();
+        //String title = driver.getTitle();
 
         //Print page's title
-        System.out.println("Page Title: " + title);
+      //  System.out.println("Page Title: " + title);
 
         //Assertion
-        Assert.assertEquals(title, "Software Test Academy", "Title assertion is failed!");
+        //Assert.assertEquals(title, "Software Test Academy", "Title assertion is failed!");
     }
 
     //-----------------------------------Test TearDown-----------------------------------
     @AfterMethod
     public void teardownTest (){
         //Close browser and end the session
-        driver.quit();
+        //driver.quit();
     }
 }
